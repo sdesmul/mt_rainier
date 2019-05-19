b32bb7eeccea526d80927a6be35c1eefc2b5b7c4
Quill McConnell <quill8197@yahoo.com>
2019-05-19 16:33:38 -0400

    Separated the header and footer into their own files and required them in the index page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Time: 2:16 PM
  */
 
-// Start seesion
+// Start session
 session_start();
 
 // Turn on error reporting
@@ -22,18 +22,22 @@ $f3 = Base::instance();
 // Turn on Fat-free error reporting
 $f3->set('DEBUG', 3);
 
-
 // require validation file
 require_once('model/validation-functions.php');
-
 
 //homepage
 $f3->route('GET /', function()
 {
+    //require the header before the contents of the page
+    require_once('views/header.html');
+
+    //display the contents of the page
     $view = new Template();
     echo $view->render("views/home_page.html");
-});
 
+    //require the footer after the contents of the page
+    require_once('views/footer.html');
+});
 
 // Run Fat-Free
 $f3->run();
